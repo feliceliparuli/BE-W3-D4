a743e48b1458bf6051c1e1bf29482d4fa45c019e
feliceliparuli <liparuli.felice50@gmail.com>
2025-05-22 21:12:37 +0200

    eercizio

---
+++ src/main/java/epicode/entities/Concerto.java
@@ -0,0 +1,51 @@
+package epicode.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "concerti")
+@NamedQuery(name = "get_concerti_in_streaming", query = "SELECT c FROM Concerto c WHERE c.inStreaming=:inStreaming")
+public class Concerto extends Event {
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Genere genere;
+    @Column(nullable = false)
+    private boolean inStreaming;
+
+    public Concerto() {
+    }
+
+    public Concerto(String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento,
+                    int numeroMassimoPartecipanti, Location location, Genere genere, boolean inStreaming) {
+        super(titolo, dataEvento, descrizione, tipoEvento, numeroMassimoPartecipanti, location );
+        this.genere = genere;
+        this.inStreaming = inStreaming;
+    }
+
+
+    public Genere getGenere() {
+        return genere;
+    }
+
+    public void setGenere(Genere genere) {
+        this.genere = genere;
+    }
+
+    public boolean isInStreaming() {
+        return inStreaming;
+    }
+
+    public void setInStreaming(boolean inStreaming) {
+        this.inStreaming = inStreaming;
+    }
+
+    @Override
+    public String toString() {
+        return "Concerto{" +
+                "id=" + getId() +
+                ", genere=" + genere +
+                ", in streaming=" + inStreaming +
+                '}';
+    }
+}
---
+++ src/main/java/epicode/entities/Genere.java
@@ -0,0 +1,5 @@
+package epicode.entities;
+
+public enum Genere {
+    CLASSICO, ROCK, POP
+}
